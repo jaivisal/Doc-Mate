a714d263e34bb71f4336ce54cba0adac8e788236
jaivisal <138554017+jaivisal@users.noreply.github.com>
2025-03-04 17:56:53 +0530

    minor fix in backend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         'auto':'repeat(auto-fill, minmax(200px, 1fr))'
       },
       colors:{
-        'primary':'#5F6FFF'
+        'primary':'#EF4444' // Hex code for red-500
       }
     },
   },
